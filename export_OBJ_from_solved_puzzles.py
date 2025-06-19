30a507fcfb63a55d39cf8c60e23ae538831a2b2c
freerafiki <=>
2025-06-19 16:49:01 +0200

    last version

--- export_OBJ_from_solved_puzzles.py
+++ export_OBJ_from_solved_puzzles.py
@@ -10,14 +10,17 @@ import pandas as pd
 from natsort import natsorted
 import shutil 
 
-root_path = '/home/lucap/code/RePair_3D_new' #'/media/lucap/big_data/datasets/repair/ground_truth'
-solved_puzzles = os.path.join(root_path, 'DONE')
-solved_puzzles_GT = os.path.join(root_path, 'PUZZLES')
-open_discovery_folder = os.path.join(solved_puzzles_GT, 'OPEN_DISCOVERY')
-solved_puzzles_json = os.path.join(root_path, 'JSON')
-os.makedirs(solved_puzzles_GT, exist_ok=True)
-os.makedirs(solved_puzzles_json, exist_ok=True)
+root_path = '/run/user/1000/gvfs/sftp:host=gpu1.dsi.unive.it,user=luca.palmieri/home/ssd/datasets/RePAIR_v2'
+#'/home/lucap/code/RePair_3D_new' #'/media/lucap/big_data/datasets/repair/ground_truth'
+solved_puzzles = os.path.join(root_path, '1_Blender_Assembled')
+exported_OBJ_folder = os.path.join(root_path, '2_Exported_OBJ')
+open_discovery_folder = os.path.join(exported_OBJ_folder, 'OPEN_DISCOVERY')
+solved_puzzle_folder = os.path.join(exported_OBJ_folder, 'SOLVED')
+json_folder = os.path.join(exported_OBJ_folder, 'JSON')
+os.makedirs(exported_OBJ_folder, exist_ok=True)
+os.makedirs(solved_puzzle_folder, exist_ok=True)
 os.makedirs(open_discovery_folder, exist_ok=True)
+os.makedirs(json_folder, exist_ok=True)
 
 list_of_solved_puzzles_unsorted = [sp for sp in os.listdir(solved_puzzles) if sp.endswith('blend')]
 list_of_solved_puzzles = natsorted(list_of_solved_puzzles_unsorted)
@@ -45,63 +48,128 @@ groups = [] #[62, 73, 74, 75]: #
 isolated = []
 isolated_groups = [] # marked as isolated collections! [62, 73, 74, 75]
 skip = [76]
-puzzle_counter = 1
+puzzle_counter = 49
 isolated_counter = 0
 
 # breakpoint()
 
 for solved_puzzle in list_of_solved_puzzles:
 
-    # breakpoint()
-    print("#" * 50)
-    print("exporting from", solved_puzzle)
+    group_num = int(solved_puzzle.split("_")[1])
+    if group_num > 47: #1 > 0: #solved_puzzle == 'group_82_DONE_new.blend':
 
-    # folder for the solved pieces 
-    group_num = int(solved_puzzle.split('_')[1])
-    # group_name = solved_puzzle[:-]
-    
-    print("1. Opening Blender file..")
-    bpy.ops.wm.open_mainfile(filepath=os.path.join(solved_puzzles, solved_puzzle))
-    bpy.data.use_autopack = True
-    bpy.context.scene.unit_settings.scale_length = 0.001
-    bpy.context.scene.unit_settings.length_unit = 'MILLIMETERS'
+        # puzzle_counter = 107
+        # breakpoint()
+        print("#" * 50)
+        print("exporting from", solved_puzzle)
 
-    # print("2. Unpacking images..")
-    # # Iterate through all images in the scene
-    # for img in bpy.data.images:
-    #     # Check if the image is packed
-    #     if img.packed_file:
-    #         # Unpack the image
-    #         img.unpack()
-    
-    gt_dict = {}
+        # folder for the solved pieces 
+        group_num = int(solved_puzzle.split('_')[1])
+        # group_name = solved_puzzle[:-]
+        
+        print("1. Opening Blender file..")
+        bpy.ops.wm.open_mainfile(filepath=os.path.join(solved_puzzles, solved_puzzle))
+        bpy.data.use_autopack = True
+        bpy.context.scene.unit_settings.scale_length = 0.001
+        bpy.context.scene.unit_settings.length_unit = 'MILLIMETERS'
 
-    
-    collections = bpy.data.collections 
-    print("Collections:")
-    for collection in collections:
-        print('-' * 30)
-        print(f"\t{collection.name}")
+        # print("2. Unpacking images..")
+        # # Iterate through all images in the scene
+        # for img in bpy.data.images:
+        #     # Check if the image is packed
+        #     if img.packed_file:
+        #         # Unpack the image
+        #         img.unpack()
+        
         gt_dict = {}
-        if "O" in collection.name or "Collection" in collection.name \
-            and ".00" not in collection.name:
 
-            if len(collection.all_objects) > 0:
+        
+        collections = bpy.data.collections 
+        print("Collections:")
+        for collection in collections:
+            print('-' * 30)
+            print(f"\t{collection.name}")
+            gt_dict = {}
+            if "O" in collection.name or "Collection" in collection.name \
+                and ".00" not in collection.name:
 
-                puzzle_name = f"puzzle_{puzzle_counter:07d}_RP_group_{group_num}"
-                print("- Creating", puzzle_name)
-                solved_pieces_folder = os.path.join(solved_puzzles_GT, puzzle_name)
-                os.makedirs(solved_pieces_folder, exist_ok=True)
-                json_name_v1 = f"RPobj_g{group_num}_o{puzzle_counter:04d}.json"
-                target_gt_path = os.path.join(solved_puzzles_json, json_name_v1)
-                puzzle_counter += 1
+                if len(collection.all_objects) > 0:
+
+                    puzzle_name = f"puzzle_{puzzle_counter:07d}_RP_group_{group_num}"
+                    print("- Creating", puzzle_name)
+                    solved_pieces_folder = os.path.join(solved_puzzle_folder, puzzle_name)
+                    os.makedirs(solved_pieces_folder, exist_ok=True)
+                    json_name_v1 = f"RPobj_g{group_num}_o{puzzle_counter:04d}.json"
+                    target_gt_path = os.path.join(json_folder, json_name_v1)
+                    
+                    
+                    # json_name_rs = f"__RP_g{group_num}_o_{unique_random_strings[puzzle_counter]}.json"
+                    # target_gt_path_rs = os.path.join(solved_puzzles_gt, json_name_rs)
+                    num_frags = 0
+                    for obj in collection.all_objects:
+                        if "RPf" in obj.name:
+                            num_frags += 1
+                            loc = obj.location 
+                            rot_euler = obj.rotation_euler
+                            rot_quat = rot_euler.to_quaternion()
+                            gt_piece = {
+                                'location': [loc[0], loc[1], loc[2]],
+                                'rotation_euler': [rot_euler.x, rot_euler.y, rot_euler.z],
+                                'rotation_quaternion': [rot_quat[0], rot_quat[1], rot_quat[2], rot_quat[3]]
+                            }
+                            gt_dict[f"{obj.name}"] = gt_piece
+                            frag_names.append(obj.name)
+                            frags_id.append(f"{obj.name[4:]}")
+                            objs.append(puzzle_counter)
+                            objs_names.append(json_name_v1)
+                            # objs_names_rs.append(json_name_rs)
+                            groups.append(group_num)
+                            isolated.append(0)
+
+                            # Saving the obj file
+                            bpy.ops.object.select_all(action='DESELECT')
+                            bpy.data.objects[obj.name].select_set(True)
+                            filepath = os.path.join(solved_pieces_folder, f'{obj.name}.obj')
+                            for mat in bpy.data.objects[obj.name].material_slots:
+                                if mat.material.use_nodes:
+                                    for node in mat.material.node_tree.nodes:
+                                        if node.type == 'TEX_IMAGE':
+                                            if node.image.packed_file:
+                                                os.chdir(solved_pieces_folder)
+                                                orig_path = bpy.path.abspath(node.image.filepath)
+                                                node.image.unpack(method='WRITE_LOCAL')
+                                                src_path = os.path.join(solved_puzzles, 'textures', os.path.basename(node.image.filepath))
+                                                dest_path = os.path.join(solved_pieces_folder, os.path.basename(node.image.filepath))
+                                                shutil.copy2(src_path, dest_path)
+                                                node.image.filepath = dest_path
+                                                node.image.reload()
+                            bpy.ops.wm.obj_export(filepath=filepath, 
+                                                export_selected_objects=True, 
+                                                path_mode='COPY',
+                                                export_materials=True)
                 
-                # json_name_rs = f"__RP_g{group_num}_o_{unique_random_strings[puzzle_counter]}.json"
-                # target_gt_path_rs = os.path.join(solved_puzzles_gt, json_name_rs)
-                num_frags = 0
+                    puzzle_names.append(puzzle_name)
+                    puzzle_num_fragments.append(num_frags)
+                    puzzle_decor.append("")
+                    puzzle_motives.append("")
+                    puzzle_description.append("")
+                    puzzle_backside.append("")
+
+                    print(f"saving puzzle {puzzle_counter} from group {group_num}")
+                    puzzle_counter += 1
+                    if puzzle_counter < group_num:
+                        print("WARNING! What happened?")
+                    with open(target_gt_path, 'w') as jtp:
+                        json.dump(gt_dict, jtp, indent=3)
+                    # with open(target_gt_path_rs, 'w') as jtp:
+                    #     json.dump(gt_dict, jtp, indent=3)
+
+            elif "isolated" in collection.name:
+                
                 for obj in collection.all_objects:
+                    gt_dict = {}
                     if "RPf" in obj.name:
-                        num_frags += 1
+                        isolated_counter += 1
                         loc = obj.location 
                         rot_euler = obj.rotation_euler
                         rot_quat = rot_euler.to_quaternion()
@@ -110,6 +178,8 @@ for solved_puzzle in list_of_solved_puzzles:
                             'rotation_euler': [rot_euler.x, rot_euler.y, rot_euler.z],
                             'rotation_quaternion': [rot_quat[0], rot_quat[1], rot_quat[2], rot_quat[3]]
                         }
+                        json_name_v1 = f"RPobj_g{group_num}_i{isolated_counter:04d}.json"
+                        # json_name_rs = f"__RP_g{group_num}_i_{unique_random_strings_I[isolated_counter]}.json"
                         gt_dict[f"{obj.name}"] = gt_piece
                         frag_names.append(obj.name)
                         frags_id.append(f"{obj.name[4:]}")
@@ -117,128 +187,66 @@ for solved_puzzle in list_of_solved_puzzles:
                         objs_names.append(json_name_v1)
                         # objs_names_rs.append(json_name_rs)
                         groups.append(group_num)
-                        isolated.append(0)
+                        isolated.append(1)
 
+                        target_gt_path = os.path.join(json_folder, json_name_v1)
+                        # target_gt_path_rs = os.path.join(solved_puzzles_gt, json_name_rs)
+                        
+                        print(f"saving isolated fragment n. {isolated_counter} from group{group_num}")
                         # Saving the obj file
                         bpy.ops.object.select_all(action='DESELECT')
                         bpy.data.objects[obj.name].select_set(True)
-                        filepath = os.path.join(solved_pieces_folder, f'{obj.name}.obj')
+                        filepath = os.path.join(open_discovery_folder, f'{obj.name}.obj')
                         for mat in bpy.data.objects[obj.name].material_slots:
                             if mat.material.use_nodes:
                                 for node in mat.material.node_tree.nodes:
                                     if node.type == 'TEX_IMAGE':
                                         if node.image.packed_file:
-                                            os.chdir(solved_pieces_folder)
+                                            os.chdir(open_discovery_folder)
                                             orig_path = bpy.path.abspath(node.image.filepath)
                                             node.image.unpack(method='WRITE_LOCAL')
                                             src_path = os.path.join(solved_puzzles, 'textures', os.path.basename(node.image.filepath))
-                                            dest_path = os.path.join(solved_pieces_folder, os.path.basename(node.image.filepath))
+                                            dest_path = os.path.join(open_discovery_folder, os.path.basename(node.image.filepath))
                                             shutil.copy2(src_path, dest_path)
                                             node.image.filepath = dest_path
                                             node.image.reload()
                         bpy.ops.wm.obj_export(filepath=filepath, 
-                                             export_selected_objects=True, 
-                                             path_mode='COPY',
-                                             export_materials=True)
-            
-                puzzle_names.append(puzzle_name)
-                puzzle_num_fragments.append(num_frags)
-                puzzle_decor.append("")
-                puzzle_motives.append("")
-                puzzle_description.append("")
-                puzzle_backside.append("")
-
-                print(f"saving puzzle {puzzle_counter} from group {group_num}")
-                if puzzle_counter < group_num:
-                    print("WARNING! What happened?")
-                with open(target_gt_path, 'w') as jtp:
-                    json.dump(gt_dict, jtp, indent=3)
-                # with open(target_gt_path_rs, 'w') as jtp:
-                #     json.dump(gt_dict, jtp, indent=3)
-
-        elif "isolated" in collection.name:
-            
-            for obj in collection.all_objects:
-                gt_dict = {}
-                if "RPf" in obj.name:
-                    isolated_counter += 1
-                    loc = obj.location 
-                    rot_euler = obj.rotation_euler
-                    rot_quat = rot_euler.to_quaternion()
-                    gt_piece = {
-                        'location': [loc[0], loc[1], loc[2]],
-                        'rotation_euler': [rot_euler.x, rot_euler.y, rot_euler.z],
-                        'rotation_quaternion': [rot_quat[0], rot_quat[1], rot_quat[2], rot_quat[3]]
-                    }
-                    json_name_v1 = f"RPobj_g{group_num}_i{isolated_counter:04d}.json"
-                    # json_name_rs = f"__RP_g{group_num}_i_{unique_random_strings_I[isolated_counter]}.json"
-                    gt_dict[f"{obj.name}"] = gt_piece
-                    frag_names.append(obj.name)
-                    frags_id.append(f"{obj.name[4:]}")
-                    objs.append(puzzle_counter)
-                    objs_names.append(json_name_v1)
-                    # objs_names_rs.append(json_name_rs)
-                    groups.append(group_num)
-                    isolated.append(1)
-
-                    target_gt_path = os.path.join(solved_puzzles_json, json_name_v1)
-                    # target_gt_path_rs = os.path.join(solved_puzzles_gt, json_name_rs)
-                    
-                    print(f"saving isolated fragment n. {isolated_counter} from group{group_num}")
-                    # Saving the obj file
-                    bpy.ops.object.select_all(action='DESELECT')
-                    bpy.data.objects[obj.name].select_set(True)
-                    filepath = os.path.join(open_discovery_folder, f'{obj.name}.obj')
-                    for mat in bpy.data.objects[obj.name].material_slots:
-                        if mat.material.use_nodes:
-                            for node in mat.material.node_tree.nodes:
-                                if node.type == 'TEX_IMAGE':
-                                    if node.image.packed_file:
-                                        os.chdir(open_discovery_folder)
-                                        orig_path = bpy.path.abspath(node.image.filepath)
-                                        node.image.unpack(method='WRITE_LOCAL')
-                                        src_path = os.path.join(solved_puzzles, 'textures', os.path.basename(node.image.filepath))
-                                        dest_path = os.path.join(open_discovery_folder, os.path.basename(node.image.filepath))
-                                        shutil.copy2(src_path, dest_path)
-                                        node.image.filepath = dest_path
-                                        node.image.reload()
-                    bpy.ops.wm.obj_export(filepath=filepath, 
-                                        export_selected_objects=True, 
-                                        path_mode='COPY',
-                                        export_materials=True)
+                                            export_selected_objects=True, 
+                                            path_mode='COPY',
+                                            export_materials=True)
 
 
-                    with open(target_gt_path, 'w') as jtp:
-                        json.dump(gt_dict, jtp, indent=3)
-                    # with open(target_gt_path_rs, 'w') as jtp:
-                    #     json.dump(gt_dict, jtp, indent=3)
+                        with open(target_gt_path, 'w') as jtp:
+                            json.dump(gt_dict, jtp, indent=3)
+                        # with open(target_gt_path_rs, 'w') as jtp:
+                        #     json.dump(gt_dict, jtp, indent=3)
+            else:
+                print(f"Group {group_num} should be there but not found!")
         else:
-            print(f"Group {group_num} should be there but not found!")
-    else:
-        print(f"Group {group_num} not found or not to be used!")
+            print(f"Group {group_num} not found or not to be used!")
 
 
-    print("finished with", solved_puzzle)
-    print("#" * 50)
-    print("\n\n\n")
+        print("finished with", solved_puzzle)
+        print("#" * 50)
+        print("\n\n\n")
 
-groups2obj_dict['pieces_names'] = frag_names
-groups2obj_dict['pieces_id'] = frags_id
-groups2obj_dict['object'] = objs
-groups2obj_dict['object_name'] = objs_names
-# groups2obj_dict['object_name_random'] = objs_names_rs
-groups2obj_dict['object'] = objs
-groups2obj_dict['group'] = groups
-groups2obj_dict['isolated'] = isolated
-groups2obj_dict.to_csv(os.path.join(solved_puzzles_GT, "fragments.csv"))
+# groups2obj_dict['pieces_names'] = frag_names
+# groups2obj_dict['pieces_id'] = frags_id
+# groups2obj_dict['object'] = objs
+# groups2obj_dict['object_name'] = objs_names
+# # groups2obj_dict['object_name_random'] = objs_names_rs
+# groups2obj_dict['object'] = objs
+# groups2obj_dict['group'] = groups
+# groups2obj_dict['isolated'] = isolated
+# groups2obj_dict.to_csv(os.path.join(solved_puzzles_GT, "fragments.csv"))
 
-puzzle_df['name'] = puzzle_names
-puzzle_df['num_fragments'] = puzzle_num_fragments
-puzzle_df['puzzle_decor'] = puzzle_decor
-puzzle_df['puzzle_motives'] = puzzle_motives
-puzzle_df['puzzle_description'] = puzzle_description
-puzzle_df['puzzle_backside'] = puzzle_backside
-puzzle_df.to_csv(os.path.join(solved_puzzles_GT, "puzzles.csv"))
+# puzzle_df['name'] = puzzle_names
+# puzzle_df['num_fragments'] = puzzle_num_fragments
+# puzzle_df['puzzle_decor'] = puzzle_decor
+# puzzle_df['puzzle_motives'] = puzzle_motives
+# puzzle_df['puzzle_description'] = puzzle_description
+# puzzle_df['puzzle_backside'] = puzzle_backside
+# puzzle_df.to_csv(os.path.join(solved_puzzles_GT, "puzzles.csv"))
 
 
     # # NOT SURE
